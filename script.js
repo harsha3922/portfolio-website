55d519d4999e050ff613a30a1d48d912842b4c47
harshthakur <149253806+harsha3922@users.noreply.github.com>
2025-07-03 07:24:47 -1200

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Welcome to Harsh's Portfolio!");
+
+  document.getElementById("download-resume").addEventListener("click", function (event) {
+    event.preventDefault(); // Stop the download for now
+
+    // Show alert
+    const confirmDownload = confirm("Are you sure you want to download the resume?");
+
+    // If user clicks OK, proceed with download
+    if (confirmDownload) {
+      window.location.href = this.href;
+    }
+  });
+
+
+
